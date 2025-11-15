8a0a14826298882c5e9227ddaf0fe8f20b417473
Sai Sandeep Rangisetti <sandeep.sai@flipkart.com>
2025-11-15 19:51:14 +0530

    Try connection per query

--- derby-async-audit/src/main/java/com/flipkart/grayskull/app/audit/DerbyDao.java
+++ derby-async-audit/src/main/java/com/flipkart/grayskull/app/audit/DerbyDao.java
@@ -6,7 +6,7 @@ import com.flipkart.grayskull.spi.models.AuditEntry;
 import io.micrometer.core.annotation.Counted;
 import io.micrometer.core.annotation.Timed;
 import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
@@ -15,20 +15,17 @@ import java.util.*;
 @Timed
 @Counted
 @Slf4j
+@AllArgsConstructor
 public class DerbyDao {
     private static final String TABLE_ALREADY_EXISTS = "X0Y32";
 
-    private final Connection connection;
+    private final String derbyUrl;
     private final ObjectMapper objectMapper;
-
-    public DerbyDao(String derbyUrl, ObjectMapper objectMapper) throws SQLException {
-        this.connection = DriverManager.getConnection(derbyUrl);
-        this.objectMapper = objectMapper;
-    }
 
     @PostConstruct
     public void init() throws SQLException {
-        try (Statement statement = connection.createStatement()) {
+        try (Connection connection = DriverManager.getConnection(derbyUrl);
+                Statement statement = connection.createStatement()) {
             statement.execute("CREATE TABLE audits (id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1), event LONG VARCHAR)");
         } catch (SQLException e) {
             if (!TABLE_ALREADY_EXISTS.equals(e.getSQLState())) {
@@ -37,13 +34,9 @@ public class DerbyDao {
         }
     }
 
-    @PreDestroy
-    public void cleanup() throws SQLException {
-        connection.close();
-    }
-
     public void insertAuditEntry(AuditEntry auditEntry) throws SQLException, JsonProcessingException {
-        try (PreparedStatement statement = connection.prepareStatement("INSERT INTO audits (event) VALUES (?)")) {
+        try (Connection connection = DriverManager.getConnection(derbyUrl);
+                PreparedStatement statement = connection.prepareStatement("INSERT INTO audits (event) VALUES (?)")) {
             String eventString = objectMapper.writeValueAsString(auditEntry);
             statement.setString(1, eventString);
             statement.execute();
@@ -51,14 +44,16 @@ public class DerbyDao {
     }
 
     public void deleteAuditEntries(long maxId) throws SQLException {
-        try (PreparedStatement statement = connection.prepareStatement("DELETE FROM audits WHERE id <= ?")) {
+        try (Connection connection = DriverManager.getConnection(derbyUrl);
+                PreparedStatement statement = connection.prepareStatement("DELETE FROM audits WHERE id <= ?")) {
             statement.setLong(1, maxId);
             statement.execute();
         }
     }
 
     public Map<Long, AuditEntry> fetchAuditEntries(long maxId, int batchSize) throws SQLException, JsonProcessingException {
-        try (PreparedStatement statement = connection.prepareStatement("SELECT id, event FROM audits WHERE id > ? ORDER BY id FETCH FIRST ? ROWS ONLY")) {
+        try (Connection connection = DriverManager.getConnection(derbyUrl);
+                PreparedStatement statement = connection.prepareStatement("SELECT id, event FROM audits WHERE id > ? ORDER BY id FETCH FIRST ? ROWS ONLY")) {
             statement.setLong(1, maxId);
             statement.setInt(2, batchSize);
             statement.execute();
